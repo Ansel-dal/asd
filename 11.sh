4fdf792afbf1175b0ebe283c110068b2fc67433f
Ansel-dal <82544423+Ansel-dal@users.noreply.github.com>
2022-08-28 22:41:40 -0300

    Create 11.sh

---
+++ 11.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#create dcmlocker.service
+echo -e "[Unit]
+Description=dcmlocker 
+[Service]
+ WorkingDirectory=/var/www/DCMLocker
+ ExecStart=/opt/dotnet/dotnet /var/www/DCMLocker/DCMLocker.Server.dll
+ Restart=always   
+ SyslogIdentifier=dotnet-dcmlocker    
+ User=root
+ Environment=ASPNETCORE_ENVIRONMENT=Production 
+[Install]
+ WantedBy=multi-user.target
+" >>  /etc/systemd/system/dcmlocker.service
