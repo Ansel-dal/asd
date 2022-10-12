a6e7a00fe69fc7eb5b4ad60ad3f93e63b3d7653d
Ansel-dal <82544423+Ansel-dal@users.noreply.github.com>
2022-10-12 13:03:46 -0300

    Update 12.sh

--- 12.sh
+++ 12.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-#doy permisos para crear dcmdigitalsignage.service 
-sudo chmod ugo+rwx /etc/systemd/system/
 
 #creo dcmdigitalsignage.service 
-echo -e "[Unit]
-Description=dcmdigitalsignage 
-[Service]
- WorkingDirectory=/home/pi/DCMDigitalSignagev2
- ExecStart=/opt/dotnet/dotnet /home/pi/DCMDigitalSignagev2
- Restart=always   
- SyslogIdentifier=dotnet-dcmdigitalsignage    
- User=root
- Environment=ASPNETCORE_ENVIRONMENT=Production 
-[Install]
- WantedBy=multi-user.target
+echo -e "HOLA HOLA
 
-" >> /etc/systemd/system/dcmdigitalsignage.service
+" >> /etc/dhcpcd.conf
