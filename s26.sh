808fdac562181703141c32610146008f4a458e30
Ansel-dal <82544423+Ansel-dal@users.noreply.github.com>
2022-11-22 10:13:14 -0300

    Update s26.sh

--- s26.sh
+++ s26.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo touch /etc/xdg/autostart/display.desktop
-sudo echo -e "Aa" >> "sudo /etc/xdg/autostart/display.desktop"
+sudo echo "Aa" >> sudo /etc/xdg/autostart/display.desktop
