ed15a6a86e75f3c2aab5f2f2d69db088058e71b1
Ansel-dal <82544423+Ansel-dal@users.noreply.github.com>
2022-11-22 10:12:10 -0300

    Update s25.sh

--- s25.sh
+++ s25.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 sudo touch /etc/xdg/autostart/display.desktop
-sudo echo -e [Desktop Entry]
-Name=KioskMode #name
-Exec=chromium --start-fullscreen --force-device-scale-factor=0.7 --disable-pinch  --kiosk --app=http://localhost:5022/ >> sudo /etc/xdg/autostart/display.desktop
+sudo echo -e aaa >> sudo /etc/xdg/autostart/display.desktop
