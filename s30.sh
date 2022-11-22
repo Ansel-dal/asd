733235c9fc3c18b0980a12fed2c7c8d1dc2bafc7
Ansel-dal <82544423+Ansel-dal@users.noreply.github.com>
2022-11-22 10:18:09 -0300

    Update and rename s29.sh to s30.sh

---
+++ s30.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo touch /etc/xdg/autostart/display.desktop
+echo -e "
+[Desktop Entry]
+Name=KioskMode #name
+Exec=chromium --start-fullscreen --force-device-scale-factor=0.7 --disable-pinch  --kiosk --app=http://localhost:5022/
+" >> /etc/xdg/autostart/display.desktop
