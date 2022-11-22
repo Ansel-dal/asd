3839de6372e87c10de57b9ae3254e64f4291da82
Ansel-dal <82544423+Ansel-dal@users.noreply.github.com>
2022-11-22 09:30:21 -0300

    Update s1.sh

--- s1.sh
+++ s1.sh
@@ -2,4 +2,4 @@ sudo sh -c"
 [Desktop Entry]
 Name=KioskMode #name
 Exec=chromium --start-fullscreen --force-device-scale-factor=0.7 --disable-pinch  --kiosk --app=http://localhost:5022/
-" >> /etc/xdg/autostart/display.desktop
+ >> /etc/xdg/autostart/display.desktop"
