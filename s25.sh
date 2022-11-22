60bb24e219c3f527b40f9f06ceed18774b7ee54e
Ansel-dal <82544423+Ansel-dal@users.noreply.github.com>
2022-11-22 10:12:49 -0300

    Update and rename s25.sh to s26.sh

--- s25.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo touch /etc/xdg/autostart/display.desktop
-sudo echo -e aaa >> sudo /etc/xdg/autostart/display.desktop
---
+++ s26.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo touch /etc/xdg/autostart/display.desktop
+sudo echo -e "Aa" >> "sudo /etc/xdg/autostart/display.desktop"
