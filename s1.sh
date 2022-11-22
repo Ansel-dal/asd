f7cc07fda4c5b64c1643632ba7121f4fb88455af
Ansel-dal <82544423+Ansel-dal@users.noreply.github.com>
2022-11-22 09:29:40 -0300

    Update s1.sh

--- s1.sh
+++ s1.sh
@@ -1,4 +1,4 @@
-sudo /bin/su echo -e "
+sudo sh -c"
 [Desktop Entry]
 Name=KioskMode #name
 Exec=chromium --start-fullscreen --force-device-scale-factor=0.7 --disable-pinch  --kiosk --app=http://localhost:5022/
