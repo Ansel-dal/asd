b805bbfc38018d36a72ee086247451ca69b49593
Ansel-dal <82544423+Ansel-dal@users.noreply.github.com>
2022-11-22 09:43:10 -0300

    Update and rename s1.sh to s5.sh

--- s5.sh
+++ s5.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo echo -e"
+sudo nano /etc/xdg/autostart/display.desktop
+echo -e "
 [Desktop Entry]
 Name=KioskMode #name
 Exec=chromium --start-fullscreen --force-device-scale-factor=0.7 --disable-pinch  --kiosk --app=http://localhost:5022/
-"
- >> /etc/xdg/autostart/display.desktop
+" >> /etc/xdg/autostart/display.desktop
