19b03b23655d04594365a88803346c72b47a76d8
Ansel-dal <82544423+Ansel-dal@users.noreply.github.com>
2022-11-24 22:52:36 -0300

    Update and rename s5.sh to s6.sh

--- s6.sh
+++ s6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo -e "
+echo "
 [Desktop Entry]
 Name=KioskMode #name
 Exec=chromium --start-fullscreen --force-device-scale-factor=0.7 --disable-pinch  --kiosk --app=http://localhost:5022/
-" >> /etc/xdg/autostart/display.desktop
+" > /etc/xdg/autostart/display.desktop
